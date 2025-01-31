2bfa93abf16ad7a5337e4bfb730554e567b1043a
ᴀᴍᴍᴀʀ ɴᴀɢᴀʀᴊɪ <ammar@nagarji.in>
2025-01-31 15:40:08 +0530

    Update plrun.sh

--- PL/plrun.sh
+++ PL/plrun.sh
@@ -1,3 +1,3 @@
 xhost +
 
-sudo docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -p 5000:5000 --name pylabv-container pylab
+sudo docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -p 5000:5000 --name pylabv-container 192.168.104.2:5000/pylab
