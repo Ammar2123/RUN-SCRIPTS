0b66ac61cc66203d36e3ee0dc48c007fd2919a1f
ᴀᴍᴍᴀʀ ɴᴀɢᴀʀᴊɪ <ammar@nagarji.in>
2025-04-04 12:25:52 +0530

    Update nlrun.sh

--- NL/nlrun.sh
+++ NL/nlrun.sh
@@ -19,4 +19,4 @@ Run Command:
 
 xhost +
 
-sudo docker run --rm -it     --name networking-lab     --net=host     --cap-add=NET_ADMIN     --cap-add=NET_RAW     --cap-add=SYS_ADMIN     --cap-add=SYS_PTRACE     -v /var/run/docker.sock:/var/run/docker.sock     -v /tmp/.X11-unix:/tmp/.X11-unix     -v $HOME/kathara_labs:/home/netuser/kathara_labs     -e DISPLAY=$DISPLAY     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native     --privileged     --user root     apsit-docker-repo.sanskrutimhatre.tech/network-lab/network-lab /bin/bash
+sudo docker run --rm -it     --name networking-lab     --net=host     --cap-add=NET_ADMIN     --cap-add=NET_RAW     --cap-add=SYS_ADMIN     --cap-add=SYS_PTRACE     -v /var/run/docker.sock:/var/run/docker.sock     -v /tmp/.X11-unix:/tmp/.X11-unix     -v $HOME/kathara_labs:/home/netuser/kathara_labs     -e DISPLAY=$DISPLAY     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native     --privileged     --user root     apsit-docker-repo.sanskrutimhatre.tech/network-lab /bin/bash
